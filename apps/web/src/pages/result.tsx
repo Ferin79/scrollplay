24334f9337d76b1822fdc79bbeadc86cbd577115
ferin79 <ferinpatel79@gmail.com>
2023-04-10 13:18:41 -0300

    result page updated

--- apps/web/src/pages/result.tsx
+++ apps/web/src/pages/result.tsx
@@ -1,10 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context, ContextType } from "../data/context";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Result = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const { isDarkMode } = useContext(Context) as ContextType;
+  const [isSuccessed, setIsSuccessed] = useState(false);
+
+  useEffect(() => {
+    if (!location.state || !location.state.data) {
+      navigate("/");
+    }
+    setIsSuccessed(true);
+  }, [location.state, navigate]);
+
+  if (!isSuccessed) {
+    return (
+      <div
+        className={`w-full h-[90vh] flex justify-center items-center ${
+          isDarkMode ? "bg-gray-900" : "bg-primaryBg"
+        }`}
+      >
+        <div className="w-[80%] lg:w-1/3 h-full flex flex-col justify-center items-center">
+          <h1
+            className={`text-5xl lg:text-6xl font-bold ${
+              isDarkMode ? "text-white" : "text-black"
+            }`}
+          >
+            Something went wrong
+          </h1>
+          <h2 className="text-3xl text-scroll mt-10">Try again later.</h2>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div
@@ -17,11 +47,11 @@ const Result = () => {
         height="720"
         controls
         autoPlay
-        src={(location.state.data.url as string) || ""}
+        src={(location.state?.data?.url as string) || ""}
       />
 
       <a
-        href={(location.state.data.query.url as string) || ""}
+        href={(location.state?.data?.url as string) || ""}
         download
         className="mt-10 py-3 px-5 bg-play rounded-2xl text-white"
       >
